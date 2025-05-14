ca662f6fa7dc64b6c32f2609360a6c742af8687f
Robertxjs <robertrus2005@gmail.com>
2025-05-14 23:11:57 +0300

    wip customer

--- Domain/customer.cpp
+++ Domain/customer.cpp
@@ -2,6 +2,57 @@
 
 using namespace std;
 
-namespace Domain {
+Customer::Customer(int id, const string& firstName, const string& lastName, const string& email,
+                const Address& address, const string& notes, bool gdprDeleted)
+                    : id(id), firstName(firstName), lastName(lastName), email(email),
+                    address(address), notes(notes), gdprDeleted(gdprDeleted) {}
+
+//Get
+
+int Customer::getId() const {
+    return id;
+}
+string Customer::getFirstName() const {
+    return firstName;
+}
+string Customer::getLastName() const {
+    return lastName;
+}
+string Customer::getEmail() const {
+    return email;
+}
+Customer::Address Customer::getAddress() const {
+    return address;
+}
+string Customer::getNotes() const {
+    return notes;
+}
+bool Customer::isGdprDeleted() const {
+    return gdprDeleted;
+}
+
+//Set
+
+void Customer::setFirstName(const string& firstName) {
+    this->firstName = firstName;
+}
+void Customer::setLastName(const string& lastName) {
+    this->lastName = lastName;
+}
+void Customer::setEmail(const string& email) {
+    this->email = email;
+}
+void Customer::setAddress(const Address& address) {
+    this->address = address;
+}
+void Customer::setNotes(const string& notes) {
+    this->notes = notes;
+}
+void Customer::setGdprDeleted(bool gdprDeleted) {
+    this->gdprDeleted = gdprDeleted;
+}
+
+
+void Customer::anonymize() {
 
 }
