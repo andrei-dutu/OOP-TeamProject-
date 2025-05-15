741472033038f148848eb5186a8d2f7f8dd585ab
andrei-dutu <popandreidutu@yahoo.com>
2025-05-15 14:17:26 +0300

    update for the project Pop Andrei

--- Domain/order.h
+++ Domain/order.h
@@ -4,6 +4,9 @@
 
 #ifndef ORDER_H
 #define ORDER_H
+#include "employee.h"
+#include <string>
+#include "product.h"
 
 namespace Domain {
 
@@ -11,8 +14,39 @@ namespace Domain {
 
     private:
 
-    public:
+        int order_number;
+        Date date;
+        std::string status;
+        Product product;
+        int quantity;
+        std::string customer_email;
+        std::string employee_email;
+        int total_amount;
 
+    public:
+        Order(int order_number, Date date, std::string status, Product product, int quantity,
+            std::string customer_email, std::string employee_email) : order_number(order_number), date(date),
+            status(status), product(product), quantity(quantity), customer_email(customer_email),
+            employee_email(employee_email) {
+            total_amount = product.getPrice() * quantity;
+        }
+
+        int getOrderNumber() const;
+        Date getDate() const;
+        std::string getStatus();
+        Product getProduct();
+        int getQuantity() const;
+        std::string getCustomerEmail();
+        std::string getEmployeeEmail();
+        int getTotalAmount() const;
+        void setOrderNumber(int order_number);
+        void setDate(Date date);
+        void setStatus(const std::string& status);
+        void setProduct(const Product& product);
+        void setQuantity(int quantity);
+        void setCustomerEmail(const std::string& customer_email);
+        void setEmployeeEmail(const std::string& employee_email);
+        void setTotalAmount(int total_amount);
     };
 }
 
--- Repository/order_repository.h
+++ Repository/order_repository.h
@@ -4,9 +4,33 @@
 
 #ifndef ORDER_REPOSITORY_H
 #define ORDER_REPOSITORY_H
+#include <vector>
+#include "../Domain/order.h"
 
 namespace Repository {
 
+    class OrderRepository {
+
+    private:
+
+        std::vector<Domain::Order> order_repo;
+
+    public:
+        void addOrder(const Domain::Order& order);
+        Domain::Order searchOrder(int id) const;
+        std::vector<Domain::Order> findOrdersByEmployee(const std::string& email) const;
+        std::vector<Domain::Order> findOrdersByCustomer(std::string email);
+        std::vector<Domain::Order> findOrdersByStatus(std::string status);
+        std::vector<Domain::Order> findOrdersByYear(std::string email);
+        void modifyOrderStatus(std::string new_status);
+        void modifyOrderNumber(int new_number);
+        void modifyOrderProduct(Domain::Product new_product);
+        void modifyOrderQuantity(int new_quantity);
+        void giveOrderToAnotherEmployee(int order_id, std::string email);
+        int getTotalAmountByYear(int year);
+        int getTotalAmountByMonth(int Month);
+
+    };
 }
 
 #endif //ORDER_REPOSITORY_H
