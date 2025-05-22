63bcbf1dfdfb43c610ddcb11ae472bcf4d2d1489
Robertxjs <robertrus2005@gmail.com>
2025-05-22 16:00:42 +0300

    cutomer

--- Controller/customer_controller.cpp
+++ Controller/customer_controller.cpp
@@ -1,8 +1,28 @@
-//
-// Created by Andrei Pop on 02.05.2025.
-//
 #include "customer_controller.h"
 
-namespace Controller {
+CustomerController::CustomerController(CustomerRepository& repo) : repository(repo) {}
 
+vector<Customer>& CustomerController::getAllCustomers() {
+    return repository.getCustomers();
 }
+
+void CustomerController::addCustomer(const Customer& customer) {
+    repository.addCustomer(customer);
+}
+
+bool CustomerController::updateCustomer(int id, const Customer& updatedCustomer) {
+    return repository.updateCustomer(id, updatedCustomer);
+}
+
+bool CustomerController::deleteCustomer(int id) {
+    return repository.deleteCustomer(id);
+}
+
+Customer* CustomerController::findByEmail(const string& email) {
+    return repository.findByEmail(email);
+}
+
+int CustomerController::getCustomerCount() const {
+    return repository.getCustomerCount();
+}
+
