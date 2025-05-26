a7eeec373bc531bc3553a121a44ea40dcdd4fea5
Robert Rus <robertrus2005@gmail.com>
2025-05-26 12:21:20 +0300

    Update customer_controller.cpp

--- Controller/customer_controller.cpp
+++ Controller/customer_controller.cpp
@@ -1,8 +1,24 @@
-//
-// Created by Andrei Pop on 02.05.2025.
-//
-#include "customer_controller.h"
+#ifndef CUSTOMER_CONTROLLER_H
+#define CUSTOMER_CONTROLLER_H
 
-namespace Controller {
+#include "../Repository/customer_repository.h"
 
-}
+class CustomerController {
+
+private:
+    CustomerRepository& repository;
+
+public:
+    CustomerController(CustomerRepository& repo);
+
+    vector<Customer>& getAllCustomers();
+    void addCustomer(const Customer& customer);
+    bool updateCustomer(int id, const Customer& updatedCustomer);
+    bool deleteCustomer(int id);
+    Customer* findByEmail(const string& email);
+    int getCustomerCount() const;
+    int getActiveCustomerCount() const;
+    vector<Customer>& findByProduct(string& productName);
+};
+
+#endif
