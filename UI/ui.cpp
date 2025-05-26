f8839006fdc5cc7be6cf37f48370a01094b48ea4
andrei-dutu <popandreidutu@yahoo.com>
2025-05-26 14:23:05 +0300

    ui and cmake update

--- UI/ui.cpp
+++ UI/ui.cpp
@@ -3,5 +3,17 @@
 //
 #include "ui.h"
 namespace UI {
+    void UI::run() {
+        std::cout<<"Login:";
+        std::string email;
+        cin>>email;
+
+        if (customer_controller.findByEmail(email) != nullptr) {
+            std::string password;
+            cin>>password;
+            if (customer_controller.findByEmail(email)->getPassword() == password)
+
+        }//if (employee_controller.)
+    }
 
 }
